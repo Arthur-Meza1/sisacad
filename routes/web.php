6d8a56de73acb940b8b3611a0eabd54bd4a4b006
All23tor <acc.dummyagta1@gmail.com>
2025-10-29 01:37:07 -0500

    Deleted unneeded register page

--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,3 @@ Route::post('/login', Login::class)
 Route::post('/logout', Logout::class)
     ->middleware('auth')
     ->name('logout');
-
-Route::view('/register', 'auth.register')
-    ->middleware('guest')
-    ->name('register');
-Route::post('/register', Register::class)
-    ->middleware('guest');
