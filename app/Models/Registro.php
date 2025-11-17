959171f1c1634053f57372bb3f75a7e79d0bb68f
ediazva <ediazva@unsa.edu.pe>
2025-11-17 11:32:42 -0500

    Funciones para el tema del sustitutorio.

--- app/Models/Registro.php
+++ app/Models/Registro.php
@@ -14,6 +14,24 @@ class Registro extends Model
     ];
     public $timestamps = false;
 
+    public function getNotasParcial() {
+      $parcial1 = $this->parcial1;
+      $parcial2 = $this->parcial2;
+      if(($sust = $this->sustitutorio) != null) {
+        if($parcial1 < $parcial2) {
+          $parcial1 = $sust;
+        } else {
+          $parcial2 = $sust;
+        }
+      }
+
+      return collect([$parcial1, $parcial2, $this->parcial3]);
+    }
+
+    public function getNotasContinua() {
+      return collect([$this->continua1, $this->continua2, $this->continua3]);
+    }
+
     public function alumno() {
         return $this->belongsTo(Alumno::class);
     }
