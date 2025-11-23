b16d8755f65b0bf8a3380401979bb6d4810d2d82
All23tor <acc.dummyagta1@gmail.com>
2025-11-22 23:17:14 -0500

    Added full fake notes to each register

--- database/factories/RegistroFactory.php
+++ database/factories/RegistroFactory.php
@@ -20,14 +20,11 @@ class RegistroFactory extends Factory
       'alumno_id' => Alumno::factory(),
       'grupo_curso_id' => GrupoCurso::factory(),
       'parcial1' => $this->faker->numberBetween(1, 20),
-      'parcial2' => null,
-      //'parcial2' => $this->faker->numberBetween(1, 20),
+      'parcial2' => $this->faker->numberBetween(1, 20),
       'parcial3' => $this->faker->numberBetween(1, 20),
       'continua1' => $this->faker->numberBetween(1, 20),
-      'continua2' => null,
-      'continua3' => null,
-      // 'continua2' => $this->faker->numberBetween(1, 20),
-      // 'continua3' => $this->faker->numberBetween(1, 20),
+      'continua2' => $this->faker->numberBetween(1, 20),
+      'continua3' => $this->faker->numberBetween(1, 20),
       'sustitutorio' => null,
     ];
   }
