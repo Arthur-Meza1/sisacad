5e134bef84457bfae46ddeee1b1f52c1e7ea2aa9
ediazva <ediazva@unsa.edu.pe>
2025-12-02 23:14:02 -0500

    Alumno controla todo lo que tiene que ver con Registros

--- app/Domain/Student/Entity/Alumno.php
+++ app/Domain/Student/Entity/Alumno.php
@@ -4,8 +4,13 @@ namespace App\Domain\Student\Entity;
 
 use App\Domain\Shared\Exception\InvalidValue;
 use App\Domain\Shared\ValueObject\Id;
+use App\Domain\Shared\ValueObject\NotasContinua;
+use App\Domain\Shared\ValueObject\NotasParcial;
+use App\Domain\Shared\ValueObject\Registro;
 
 class Alumno {
+  private Registro $registro;
+
   private function __construct(
     private readonly Id $id,
     private readonly string $nombre,
@@ -24,6 +29,14 @@ class Alumno {
       id: $id,
       nombre: $nombre,
     );
+  }
+
+  public function loadRegistro(Registro $registro): void {
+    $this->registro = $registro;
+  }
+
+  public function registro(): Registro {
+    return $this->registro;
   }
 
   public function nombre(): ?string {
