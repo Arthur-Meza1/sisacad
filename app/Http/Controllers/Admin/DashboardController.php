23764c1b06b1c432700d21949c7eb2c9b9cb5d81
All23tor <acc.dummyagta1@gmail.com>
2025-12-04 04:20:17 -0500

    Fixed authentication bug caused by mixed property un User class, added view for admin

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Infrastructure\Shared\Model\User;
+use App\Infrastructure\Shared\Model\Curso;
+class DashboardController extends Controller
+{
+  public function index()
+  {
+    return view('admin', [
+      'users_count' => User::count(),
+      'courses_count' => Curso::count(),
+    ]);
+  }
+}
