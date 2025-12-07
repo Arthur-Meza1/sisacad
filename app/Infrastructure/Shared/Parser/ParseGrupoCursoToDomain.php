e1810bd142b0794afaac0e97e6aafbd917d99fef
ediazva <ediazva@unsa.edu.pe>
2025-12-07 15:08:18 -0500

    No es necesario crear un DTO, ahora solo usamos GrupoCurso y permitimos al Usuario decidir si cargar o no cargar los cursos

---
+++ app/Infrastructure/Shared/Parser/ParseGrupoCursoToDomain.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Infrastructure\Shared\Parser;
+
+use App\Domain\Shared\Entity\Curso;
+use App\Domain\Shared\Entity\GrupoCurso;
+use App\Domain\Shared\ValueObject\CursoTipo;
+use App\Domain\Shared\ValueObject\GrupoTurno;
+use App\Domain\Shared\ValueObject\Id;
+use App\Infrastructure\Shared\Model\GrupoCurso as EloquentGrupoCurso;
+
+class ParseGrupoCursoToDomain
+{
+  public static function fromEloquent(EloquentGrupoCurso $grupo): GrupoCurso {
+    return GrupoCurso::fromPrimitive(
+      id: Id::fromInt($grupo->id),
+      curso: Curso::create(
+        id: Id::fromInt($grupo->curso->id),
+        nombre: $grupo->curso->nombre,
+      ),
+      grupoTurno: GrupoTurno::fromString($grupo->turno),
+      cursoTipo: CursoTipo::fromString($grupo->tipo),
+      docenteNombre: $grupo->docente->user->name,
+    );
+  }
+}
