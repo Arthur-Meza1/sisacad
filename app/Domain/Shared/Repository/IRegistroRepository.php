e4bd0c550b5ec4bf5d0a753689f08ac89dbba403
ediazva <ediazva@unsa.edu.pe>
2025-12-17 01:10:10 -0500

    Ahora se permite notas vacias

--- app/Domain/Shared/Repository/IRegistroRepository.php
+++ app/Domain/Shared/Repository/IRegistroRepository.php
@@ -8,5 +8,6 @@ use App\Domain\Shared\ValueObject\Registro;
 interface IRegistroRepository
 {
   public function getOrCreateByAlumnoInGrupo(Id $alumnoId, Id $grupoId): Registro;
+  public function getById(Id $id): Registro;
   public function save(Registro $registro): void;
 }
