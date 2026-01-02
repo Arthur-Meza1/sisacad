63c50c76076fe3805a7af77e3b9500ecfc8ab25f
ediazva <ediazva@unsa.edu.pe>
2026-01-02 10:21:15 -0500

    Se añadió la entidad Temas

--- app/Application/Student/Transformer/GrupoCursoTransformer.php
+++ app/Application/Student/Transformer/GrupoCursoTransformer.php
@@ -4,25 +4,25 @@ namespace App\Application\Student\Transformer;
 
 use App\Domain\Shared\Entity\GrupoCurso;
 
-class GrupoCursoTransformer
-{
-    /**
-     * @param  GrupoCurso[]  $cursos
-     */
-    public static function toArray(array $cursos): array
-    {
-        $res = [];
+class GrupoCursoTransformer {
+  /**
+   * @param GrupoCurso[] $cursos
+   * @return array
+   */
+  public static function toArray(array $cursos): array {
+    $res = [];
 
-        foreach ($cursos as $curso) {
-            $res[] = [
-                'id' => $curso->id()->getValue(),
-                'nombre' => $curso->curso()->nombre(),
-                'turno' => $curso->grupoTurno()->getValue(),
-                'tipo' => $curso->cursoTipo()->getValue(),
-                'docente' => $curso->docente(),
-            ];
-        }
-
-        return $res;
+    foreach ($cursos as $curso) {
+      $res[] = [
+        'id' => $curso->id()->getValue(),
+        'nombre' => $curso->curso()->nombre(),
+        'temas' => $curso->temas()->toArray(),
+        'turno' => $curso->grupoTurno()->getValue(),
+        'tipo' => $curso->cursoTipo()->getValue(),
+        'docente' => $curso->docente()
+      ];
     }
+
+    return $res;
+  }
 }
--- app/Domain/Shared/Entity/GrupoCurso.php
+++ app/Domain/Shared/Entity/GrupoCurso.php
@@ -6,53 +6,62 @@ use App\Domain\Shared\Exception\InvalidValue;
 use App\Domain\Shared\ValueObject\CursoTipo;
 use App\Domain\Shared\ValueObject\GrupoTurno;
 use App\Domain\Shared\ValueObject\Id;
+use Illuminate\Support\Collection;
 
-readonly class GrupoCurso
-{
-    private function __construct(
-        private Id $id,
-        private Curso $curso,
-        private GrupoTurno $grupoTurno,
-        private CursoTipo $cursoTipo,
-        private string $docenteNombre,
-    ) {}
+class GrupoCurso {
+  private function __construct(
+    private readonly Id         $id,
+    private readonly Curso      $curso,
+    private readonly GrupoTurno $grupoTurno,
+    private readonly CursoTipo  $cursoTipo,
+    private readonly string     $docenteNombre,
+    private Collection          $temas,
+  ) {}
 
-    public static function fromPrimitive(
-        Id $id,
-        Curso $curso,
-        GrupoTurno $grupoTurno,
-        CursoTipo $cursoTipo,
-        string $docenteNombre,
-    ): self {
-        if (empty($docenteNombre)) {
-            throw InvalidValue::stringNullOrEmpty();
-        }
-
-        return new self($id, $curso, $grupoTurno, $cursoTipo, $docenteNombre);
+  public static function fromPrimitive(
+    Id $id,
+    Curso $curso,
+    GrupoTurno $grupoTurno,
+    CursoTipo $cursoTipo,
+    string $docenteNombre,
+  ): self {
+    if(empty($docenteNombre)) {
+      throw InvalidValue::stringNullOrEmpty();
     }
+    return new self(
+      $id,
+      $curso,
+      $grupoTurno,
+      $cursoTipo,
+      $docenteNombre,
+      collect());
+  }
 
-    public function id(): Id
-    {
-        return $this->id;
-    }
+  public function addTema(Tema $tema): void {
+    $this->temas->push($tema);
+  }
 
-    public function curso(): Curso
-    {
-        return $this->curso;
-    }
+  public function temas(): Collection {
+    return $this->temas;
+  }
 
-    public function docente(): string
-    {
-        return $this->docenteNombre;
-    }
+  public function id(): Id {
+    return $this->id;
+  }
 
-    public function grupoTurno(): GrupoTurno
-    {
-        return $this->grupoTurno;
-    }
+  public function curso(): Curso {
+    return $this->curso;
+  }
 
-    public function cursoTipo(): CursoTipo
-    {
-        return $this->cursoTipo;
-    }
+  public function docente(): string {
+    return $this->docenteNombre;
+  }
+
+  public function grupoTurno(): GrupoTurno {
+    return $this->grupoTurno;
+  }
+
+  public function cursoTipo(): CursoTipo {
+    return $this->cursoTipo;
+  }
 }
